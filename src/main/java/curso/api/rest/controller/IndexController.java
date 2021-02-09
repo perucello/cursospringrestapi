62eda2fb49e0b3d9fdb2fe40c6559771171c83a5
perucello <perucello@hotmail.com>
2021-02-08 21:17:37 -0300

    first commit

---
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -0,0 +1,134 @@
+package curso.api.rest.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repositoy.UsuarioRepository;
+
+@RestController /* Arquitetura REST */
+@RequestMapping(value = "/usuario")
+public class IndexController {
+	
+	@Autowired /* de fosse CDI seria @Inject*/
+	private UsuarioRepository usuarioRepository;
+	
+	
+	/* Serviço RESTful */
+	@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
+	public ResponseEntity<Usuario> relatorio(@PathVariable (value = "id") Long id
+			                                , @PathVariable (value = "venda") Long venda) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		/*o retorno seria um relatorio*/
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+
+	/* Serviço RESTful */
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete (@PathVariable("id") Long id){
+		
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
+	}
+	
+	
+	@DeleteMapping(value = "/{id}/venda", produces = "application/text")
+	public String deletevenda(@PathVariable("id") Long id){
+		
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
+	}
+	
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuario (){
+		
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+	
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+		
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos ++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+		
+		/*outras rotinas antes de atualizar*/
+		
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos ++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	
+	
+	@PutMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity updateVenda(@PathVariable Long iduser, 
+			                                     @PathVariable Long idvenda) {
+		/*outras rotinas antes de atualizar*/
+		
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("Venda atualzada", HttpStatus.OK);
+		
+	}
+	
+	
+	@PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces = "application/json")
+	public ResponseEntity cadastrarvenda(@PathVariable Long iduser, 
+			                                     @PathVariable Long idvenda) {
+		
+		/*Aqui seria o processo de venda*/
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("id user :" + iduser + " idvenda :"+ idvenda, HttpStatus.OK);
+		
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/curso/api/rest/model/Usuario.java
@@ -0,0 +1,101 @@
+package curso.api.rest.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String login;
+
+	private String senha;
+	
+	private String nome;
+	
+
+	@OneToMany(mappedBy="usuario", orphanRemoval = true, cascade = CascadeType.ALL, fetch =FetchType.EAGER)
+	private List<Telefone> telefones = new ArrayList<Telefone>();
+	
+	
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+	
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
